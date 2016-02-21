122267fbc52bfa35d2df14bafcd45647870e8a4e
Michael Deakin <mfdeakin@cs.unc.edu>
2016-02-21 01:29:22 -0500

    Require casts from CPrec to native fp primitives to be explicit Implement comparisons for CPrec Implement exact addition for CPrec; DoublePrec still needs work Implement more tests for comparisons, addition test

--- cdtprec.hpp
+++ cdtprec.hpp
@@ -11,44 +11,117 @@ namespace CDTPrec {
 template <typename fptype>
 class DoublePrec;
 
-template <typename fptype>
-class TriplePrec;
-
-template <typename hwfptype>
+template <typename hwfloat>
 class CPrec {
  public:
+  using single = hwfloat;
   static constexpr int getPrec() {
-    return GenericFP::fpconvert<hwfptype>::precision;
+    return GenericFP::fpconvert<single>::precision;
   }
 
-  CPrec(hwfptype val = NAN) { this->val = val; }
+  CPrec(single val = NAN) { this->val = val; }
 
-  constexpr bool operator==(float rhs) const {
-    return val == rhs;
+  template <typename fp>
+  constexpr bool operator==(fp rhs) const {
+    return val == rhs.val;
   }
 
-  constexpr bool operator==(double rhs) const {
-    return val == rhs;
+  template <typename fp>
+  constexpr bool operator<(fp rhs) const {
+    return val < rhs.val;
   }
 
-  constexpr bool operator==(long double rhs) const {
-    return val == rhs;
+  template <typename fp>
+  constexpr bool operator<=(fp rhs) const {
+    return val <= rhs.val;
   }
 
-  constexpr bool operator!=(float rhs) const {
-    return val != rhs;
+  template <typename fp>
+  constexpr bool operator>(fp rhs) const {
+    return val > rhs.val;
   }
 
-  constexpr bool operator!=(double rhs) const {
-    return val != rhs;
+  template <typename fp>
+  constexpr bool operator>=(fp rhs) const {
+    return val >= rhs.val;
   }
 
-  constexpr bool operator!=(long double rhs) const {
-    return val != rhs;
+  template <typename fp>
+  constexpr bool operator!=(fp rhs) const {
+    return val != rhs.val;
+  }
+
+  constexpr explicit operator single() const { return val; }
+
+  constexpr operator DoublePrec<CPrec<single>>() const {
+    return DoublePrec<CPrec<single>>(val, val * 0.0);
+  }
+
+  constexpr DoublePrec<CPrec<single>> operator+(
+      CPrec<single> rhs) const {
+    single larger = (val > rhs.val) ? val : rhs.val;
+    single smaller = (val < rhs.val) ? val : rhs.val;
+    single sum = larger + smaller;
+    single err = sum - larger;
+    single correct = smaller - err;
+    return DoublePrec<CPrec<single>>(
+        CPrec<single>(sum), CPrec<single>(correct));
+  }
+
+  template <typename>
+  friend class CPrec;
+
+ private:
+  single val;
+};
+
+template <typename single>
+class DoublePrec {
+ public:
+  static constexpr int getPrec() {
+    return 2 * single::getPrec();
+  }
+
+  DoublePrec(const single &greater, const single &lesser) {
+    this->greater = greater;
+    this->lesser = lesser;
+  }
+
+  constexpr single higherOrder() const { return greater; }
+  constexpr single lowerOrder() const { return lesser; }
+
+  constexpr DoublePrec<DoublePrec<single>> operator+(
+      single rhs) const {
+    single larger((greater > rhs) ? greater : rhs);
+    single smaller((greater < rhs) ? greater : rhs);
+    single sum(larger + smaller);
+    single err = sum - larger;
+    single correct = smaller - err;
+    return DoublePrec<DoublePrec<single>>(
+        DoublePrec<single>(sum),
+        DoublePrec<single>(correct));
+  }
+
+  constexpr bool operator==(DoublePrec<single> rhs) const {
+    return (greater == rhs.greater) &&
+           (lesser == rhs.lesser);
+  }
+
+  constexpr bool operator>(DoublePrec<single> rhs) const {
+    return (greater > rhs.greater) ||
+           ((greater == rhs.greater) &&
+            (lesser > rhs.greater));
+  }
+
+  constexpr operator single() const { return greater; }
+  constexpr operator DoublePrec<DoublePrec<single>>()
+      const {
+    DoublePrec<DoublePrec<single>> ret();
   }
 
  private:
-  hwfptype val;
+  single greater;
+  single lesser;
 };
 };
 #endif
--- testCPrec.cpp
+++ testCPrec.cpp
@@ -8,21 +8,75 @@ using spfloat = CDTPrec::CPrec<float>;
 using dpfloat = CDTPrec::CPrec<double>;
 using lpfloat = CDTPrec::CPrec<long double>;
 
+using spdouble = CDTPrec::DoublePrec<spfloat>;
+using dpdouble = CDTPrec::DoublePrec<dpfloat>;
+using lpdouble = CDTPrec::DoublePrec<lpfloat>;
+
 TEST(TestCPrec, Precision) {
   EXPECT_EQ(spfloat::getPrec(), 24);
   EXPECT_EQ(dpfloat::getPrec(), 53);
   EXPECT_EQ(lpfloat::getPrec(), 65);
+
+	EXPECT_EQ(spdouble::getPrec(), 48);
+	EXPECT_EQ(dpdouble::getPrec(), 106);
+	EXPECT_EQ(lpdouble::getPrec(), 130);
 }
 
-TEST(TestCPrec, Initialize) {
+TEST(TestCPrec, CompareFloat) {
   spfloat spDef;
-  EXPECT_EQ(spfloat(0.0), 0.0);
-  EXPECT_NE(spDef, NAN);
-  EXPECT_NE(spDef, 0.0);
-  EXPECT_NE(spDef, 1.0);
-  for(float val = 0.0; val != (float)(val + 1.0f); val += 1.0f) {
-    EXPECT_EQ(spfloat(val), val);
-    EXPECT_EQ(dpfloat(val), val);
-    EXPECT_EQ(lpfloat(val), val);
+  EXPECT_EQ(spfloat(0.0), spfloat(0.0));
+  EXPECT_NE(spDef, spfloat(NAN));
+  EXPECT_NE(spDef, spfloat(0.0));
+  EXPECT_NE(spDef, spfloat(1.0));
+  for(float val = 0.0; val != (float)(val + 1.0f);
+      val += 8.0f) {
+    EXPECT_EQ((float)spfloat(val), val);
+    EXPECT_EQ((double)dpfloat(val), val);
+    EXPECT_EQ((long double)lpfloat(val), val);
+
+    EXPECT_EQ(spfloat(val), spfloat(val));
+
+    EXPECT_EQ(dpfloat(val), spfloat(val));
+    EXPECT_EQ(dpfloat(val), dpfloat(val));
+    EXPECT_EQ(dpfloat(val), lpfloat(val));
+
+    EXPECT_EQ(lpfloat(val), spfloat(val));
+    EXPECT_EQ(lpfloat(val), dpfloat(val));
+    EXPECT_EQ(lpfloat(val), lpfloat(val));
+
+    EXPECT_EQ((float)spfloat(val), val);
+
+    EXPECT_EQ((spdouble)spfloat(val), spdouble(val, 0.0));
+
+    EXPECT_GT(spfloat(val), spfloat(val - 1.0f));
+    EXPECT_GT(dpfloat(val), dpfloat(val - 1.0f));
+    EXPECT_GT(lpfloat(val), lpfloat(val - 1.0f));
+
+    EXPECT_GE(spfloat(val), spfloat(val - 1.0f));
+    EXPECT_GE(dpfloat(val), dpfloat(val - 1.0f));
+    EXPECT_GE(lpfloat(val), lpfloat(val - 1.0f));
+
+    EXPECT_LT(spfloat(val), spfloat(val + 1.0f));
+    EXPECT_LT(dpfloat(val), dpfloat(val + 1.0f));
+    EXPECT_LT(lpfloat(val), lpfloat(val + 1.0f));
+
+    EXPECT_LE(spfloat(val), spfloat(val + 1.0f));
+    EXPECT_LE(dpfloat(val), dpfloat(val + 1.0f));
+    EXPECT_LE(lpfloat(val), lpfloat(val + 1.0f));
+  }
+}
+
+TEST(TestCPrec, Addition) {
+  for(float lhs = 0.0; lhs != (float)lhs + 1.0f;
+      lhs += 4096.0f) {
+    for(float rhs = 0.0; rhs != (float)rhs + 1.0f;
+        rhs += 4096.0f) {
+      spdouble sum(spfloat(lhs) + spfloat(rhs));
+      EXPECT_EQ(sum.higherOrder(), spfloat(lhs + rhs));
+			float sRounded = lhs + rhs;
+			float truncated = sRounded - ((lhs > rhs) ? lhs : rhs);
+			float err = ((lhs > rhs) ? rhs : lhs) - truncated;
+			EXPECT_EQ(sum.lowerOrder(), spfloat(err));
+    }
   }
 }
